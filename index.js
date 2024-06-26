5c6b5c62ae880ae3d0d0145b510f3fba6d2a2a66
Newton <newtonkirimi55@gmail.com>
2024-06-26 07:06:17 -0700

    arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+// // Task 1: Multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log("Multiplication:", multiply); // Output: 62
+
+// Task 2: Random
+let random = Math.floor(Math.random() * 100) + 1; // Generates a random integer between 1 and 100
+console.log("Random Number:", random); // Output: Random number greater than 0
+
+// Task 3: Modulus
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;
+console.log("Modulus:", mod); // Output: 4
+
+// Task 4: Max
+let max = Math.max(10, 20, 15); // Finds the highest number in the set
+console.log("Max Number:", max); // Output: 20
